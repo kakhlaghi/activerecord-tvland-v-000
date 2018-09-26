eb939bfd9d98c8186ebd88c3a7509b3871043070
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-09-26 01:04:44 +0000

    automatically backed up by learn

--- db/migrate/001_create_characters.rb
+++ db/migrate/001_create_characters.rb
@@ -2,7 +2,7 @@ class CreateCharacters < ActiveRecord::Migration
 
   def change
     create_table :characters do |t|
-      t.string :first_name
-      t.string :last_name
+      t.string :name
+      t.string :show_id
   end
 end
